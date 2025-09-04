69d9001a9609873eed5532c4f32167db53a15e14
cadst <tyu1234567@naver.com>
2025-09-04 14:57:04 +0900

    [update] deploy update test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>AWS 배포 test</h1>
+      <h1>AWS 배포 test update</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
